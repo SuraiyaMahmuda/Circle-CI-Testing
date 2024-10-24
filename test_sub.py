382a7014bf8d80ff9d39c521cce215a4998342eb
Suraiya Mahmuda <jucse29.364@gmail.com>
2024-10-24 12:56:33 +0600

    _

--- test_sub.py
+++ test_sub.py
@@ -2,7 +2,7 @@
 from sub import subtract
 
 def test_subtract():
-    assert subtract(10, 5) == 5
+    assert subtract(10, 5) == 6
     assert subtract(20, 0) == 20
     assert subtract(0, 10) == -10
     assert subtract(-5, -5) == 0
